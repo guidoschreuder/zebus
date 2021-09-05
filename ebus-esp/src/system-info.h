33708d84e40d17c1c1a386d23bb42b2fb5da166e
Guido Schreuder <guido@ruimtepuin.be>
2021-09-05 13:46:17 +0200

    Added system-info.h

---
+++ ebus-esp/src/system-info.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ebus-messages.h"
+
+struct system_info_t {
+  identification_t self_id;
+  identification_t heater_id;
+  bool flame = false;
+  uint16_t flow;
+  struct wifi {
+    struct config_ap {
+      const char* ap_name;
+      const char* ap_password;
+      bool active;
+    } config_ap;
+    int32_t rssi;
+  } wifi;
+};
+
+extern struct system_info_t* system_info;
