806ae3e7f165040815d222829d8dc35ff1113e03
Guido Schreuder <guido@ruimtepuin.be>
2021-09-15 19:32:33 +0200

    refactor zebus-wifi

--- zebus/src/zebus-wifi.cpp
+++ zebus/src/zebus-wifi.cpp
@@ -11,11 +11,24 @@
 
 const char PWD_CHARS[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
 
+WiFiManager wiFiManager;
+
 bool espNowInit = false;
 
 #define BEACON_INTERVAL_MS 5000
 uint32_t lastBeacon = 0;
 
+// prototype
+void setupWiFi();
+void startConfigPortal();
+void saveConfigPortalParamsCallback();
+void refreshNTP();
+void sendEspNowBeacon();
+void setupEspNow();
+void OnEspNowDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len);
+void OnEspNowDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+
+// public functions
 const char* generate_ap_password() {
   uint32_t seed = ESP.getEfuseMac() >> 32;
   static char pwd[EBUS_WIFI_CONFIG_AP_PASSWORD_LENGTH + 1] = {0};
@@ -25,9 +38,77 @@ const char* generate_ap_password() {
   return pwd;
 }
 
-WiFiManager wiFiManager;
+void wiFiLoop(void *pvParameter) {
 
-void saveParamsCallback() {
+  setupWiFi();
+  startConfigPortal();
+
+  while(1) {
+    if (system_info->wifi.config_ap.active) {
+      wiFiManager.process();
+      vTaskDelay(1);
+      continue;
+    }
+    //wiFiManager.stopConfigPortal();
+
+    printf("WiFi status: %d\n", WiFi.status());
+    if (WiFi.status() == WL_CONNECTED) {
+      system_info->wifi.rssi = WiFi.RSSI();
+      system_info->wifi.ip_addr = WiFi.localIP();
+      refreshNTP();
+      sendEspNowBeacon();
+
+      vTaskDelay(pdMS_TO_TICKS(BEACON_INTERVAL_MS));
+      continue;
+    }
+    printf("WiFi connection was lost\n");
+    system_info->wifi.rssi = WIFI_NO_SIGNAL;
+
+    ESP_ERROR_CHECK(esp_wifi_connect());
+    for (int i = 0; i < 20 && (WiFi.status() != WL_CONNECTED); i++) {
+      vTaskDelay(100);
+    }
+    if (WiFi.status() == WL_CONNECTED) {
+      printf("WiFi reconnection SUCCESS\n");
+    } else {
+      printf("WiFi reconnection FAILED\n");
+    }
+  }
+}
+
+// implementations
+void setupWiFi() {
+  WiFi.mode(WIFI_STA); // explicitly set mode, ESP32 defaults to STA+AP
+  WiFi.setAutoReconnect(false);
+
+  // disable modem sleep so ESP-NOW packets can be received
+  esp_wifi_set_ps(WIFI_PS_NONE);
+}
+
+void startConfigPortal() {
+  char apName[16] = {0};
+  sprintf(apName, "%s %x", EBUS_APPNAME, (uint32_t) ESP.getEfuseMac());
+
+  system_info->wifi.rssi = WIFI_NO_SIGNAL;
+  system_info->wifi.config_ap.ap_name = strdup(apName);
+  system_info->wifi.config_ap.ap_password = generate_ap_password();
+
+  wiFiManager.setConfigPortalBlocking(false);
+  wiFiManager.setCaptivePortalEnable(false);
+  wiFiManager.setShowInfoUpdate(false);
+  wiFiManager.setSaveParamsCallback(saveConfigPortalParamsCallback);
+  wiFiManager.setHostname("zebus.home.arpa");
+
+  system_info->wifi.config_ap.active =
+      !wiFiManager.autoConnect(system_info->wifi.config_ap.ap_name,
+                               system_info->wifi.config_ap.ap_password);
+
+  if (system_info->wifi.config_ap.active) {
+    printf("WiFi Configuration Portal is activated\n");
+  }
+}
+
+void saveConfigPortalParamsCallback() {
   printf("WiFiManager saveParamsCallback called\n");
   system_info->wifi.config_ap.active = false;
 }
@@ -39,35 +120,6 @@ void refreshNTP() {
     printf("Refreshing NTP\n");
     configTime(EBUS_NTP_GMT_OFFSET_SEC, EBUS_NTP_GMT_DST_OFFSET_SEC, EBUS_NTP_SERVER);
     system_info->ntp.last_init = now;
-  }
-}
-
-void OnEspNowDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
-  printf("Packet received from %02X:%02x:%02x:%02X:%02X:%02X\n", mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  outside_temp_message data;
-  memcpy(&data, incomingData, sizeof(data));
-  printf("Temperature: %f\n", data.temperatureC);
-}
-
-void OnEspNowDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-//  printf("Last Packet Send Status: %s\n", status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-}
-
-void setupEspNow() {
-  if (!espNowInit) {
-    ESP_ERROR_CHECK(esp_now_init());
-
-    ESP_ERROR_CHECK(esp_now_register_send_cb(OnEspNowDataSent));
-    ESP_ERROR_CHECK(esp_now_register_recv_cb(OnEspNowDataRecv));
-
-    static esp_now_peer_info_t peerInfo;
-    memcpy(peerInfo.peer_addr, espnow_broadcast_address, sizeof(espnow_broadcast_address));
-    peerInfo.channel = 0;
-    peerInfo.encrypt = false;
-
-    ESP_ERROR_CHECK(esp_now_add_peer(&peerInfo));
-
-    espNowInit = true;
   }
 }
 
@@ -105,66 +157,31 @@ void sendEspNowBeacon() {
   ESP_ERROR_CHECK(esp_wifi_connect());
 }
 
-void wiFiLoop(void *pvParameter) {
+void setupEspNow() {
+  if (!espNowInit) {
+    ESP_ERROR_CHECK(esp_now_init());
 
-  WiFi.mode(WIFI_STA); // explicitly set mode, ESP32 defaults to STA+AP
-  WiFi.setAutoReconnect(false);
+    ESP_ERROR_CHECK(esp_now_register_send_cb(OnEspNowDataSent));
+    ESP_ERROR_CHECK(esp_now_register_recv_cb(OnEspNowDataRecv));
 
-  // disable modem sleep so ESP-NOW packets can be received
-  esp_wifi_set_ps(WIFI_PS_NONE);
+    static esp_now_peer_info_t peerInfo;
+    memcpy(peerInfo.peer_addr, espnow_broadcast_address, sizeof(espnow_broadcast_address));
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
 
-  char apName[16] = {0};
-  sprintf(apName, "%s %x", EBUS_APPNAME, (uint32_t) ESP.getEfuseMac());
+    ESP_ERROR_CHECK(esp_now_add_peer(&peerInfo));
 
-  system_info->wifi.rssi = WIFI_NO_SIGNAL;
-  system_info->wifi.config_ap.ap_name = strdup(apName);
-  system_info->wifi.config_ap.ap_password = generate_ap_password();
-
-  wiFiManager.setConfigPortalBlocking(false);
-  wiFiManager.setCaptivePortalEnable(false);
-  wiFiManager.setShowInfoUpdate(false);
-  wiFiManager.setSaveParamsCallback(saveParamsCallback);
-  wiFiManager.setHostname("zebus.home.arpa");
-
-  system_info->wifi.config_ap.active =
-      !wiFiManager.autoConnect(system_info->wifi.config_ap.ap_name,
-                               system_info->wifi.config_ap.ap_password);
-  if (system_info->wifi.config_ap.active) {
-    printf("WiFi connection established\n");
+    espNowInit = true;
   }
+}
 
-  printf("Channel: %d, MAC: %s\n", WiFi.channel(), WiFi.macAddress().c_str());
+void OnEspNowDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
+  printf("Packet received from %02X:%02x:%02x:%02X:%02X:%02X\n", mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  outside_temp_message data;
+  memcpy(&data, incomingData, sizeof(data));
+  printf("Temperature: %f\n", data.temperatureC);
+}
 
-  while(1) {
-    if (system_info->wifi.config_ap.active) {
-      wiFiManager.process();
-      vTaskDelay(1);
-      continue;
-    }
-    //wiFiManager.stopConfigPortal();
-
-    printf("WiFi status: %d\n", WiFi.status());
-    if (WiFi.status() == WL_CONNECTED) {
-      system_info->wifi.rssi = WiFi.RSSI();
-      system_info->wifi.ip_addr = WiFi.localIP();
-      refreshNTP();
-      sendEspNowBeacon();
-
-      vTaskDelay(pdMS_TO_TICKS(BEACON_INTERVAL_MS));
-      continue;
-    }
-    printf("WiFi connection was lost\n");
-    system_info->wifi.rssi = WIFI_NO_SIGNAL;
-
-    ESP_ERROR_CHECK(esp_wifi_connect());
-    for (int i = 0; i < 20 && (WiFi.status() != WL_CONNECTED); i++) {
-      vTaskDelay(100);
-    }
-    if (WiFi.status() == WL_CONNECTED) {
-      printf("WiFi reconnection SUCCESS\n");
-    } else {
-      printf("WiFi reconnection FAILED\n");
-    }
-  }
-
+void OnEspNowDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+//  printf("Last Packet Send Status: %s\n", status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
