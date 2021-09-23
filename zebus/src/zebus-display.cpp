03c85375f5634a84622994c3b380ad549089d060
Guido Schreuder <guido@ruimtepuin.be>
2021-09-24 01:48:57 +0200

    restructure 'zebus-display.cpp' to use up-front declarations

--- zebus/src/zebus-display.cpp
+++ zebus/src/zebus-display.cpp
@@ -7,6 +7,9 @@
 #include "zebus-messages.h"
 #include "zebus-system-info.h"
 
+#define GET_BYTE(INT32, I) ((INT32 >> 8 * I) & 0XFF)
+
+// variables
 bool displayInit = false;
 
 TFT_eSPI tft = TFT_eSPI(); // Invoke library
@@ -24,6 +27,8 @@ uint8_t flow_cntr = 0;
 // 4-bit Palette colour table
 uint16_t palette[16];
 
+// upfront implementations
+// for now this is just to keep the defines with the palette
 void init4BitPalette() {
   palette[0]  = TFT_BLACK;
   palette[1]  = TFT_ORANGE;
@@ -54,133 +59,39 @@ void init4BitPalette() {
 #define EBUS_4BIT_YELLOW 14
 #define EBUS_4BIT_WHITE 15
 
-void initSpriteShower() {
-    spriteShower.setColorDepth(4);
-    spriteShower.createSprite(30,30);
-    spriteShower.createPalette(palette);
-    spriteShower.fillSprite(EBUS_4BIT_BLACK);
-}
+// prototypes
+void setupDisplay();
 
-void drawSpriteShower(int32_t x, int32_t y, uint8_t flow) {
-  // remove previous water
-  spriteShower.fillRect(10, 12, 20, 18, EBUS_4BIT_BLACK);
+// prototype: init
+void initSprites();
+void initSpriteShower();
+void initSpriteHeater();
+void initSpriteWifiStrength();
+void initSpriteEbusQueue();
 
-  spriteShower.fillRoundRect(16, 0, 8, 12, 1, flow ? EBUS_4BIT_RED : EBUS_4BIT_BLACK);
-  spriteShower.fillRoundRect(0, 2, 14, 4, 1, flow ? EBUS_4BIT_RED : EBUS_4BIT_BLACK);
-  if (!flow) {
-    spriteShower.drawRoundRect(16, 0, 8, 12, 1, EBUS_4BIT_WHITE);
-    spriteShower.drawRoundRect(0, 2, 14, 4, 1, EBUS_4BIT_WHITE);
-  } else {
-    for (uint8_t i = 0; i < 2 + (flow / 2); i++) {
-      uint16_t w_y = random(16);
-      uint16_t w_x = random((flow * w_y  / 512) + 3);
-      w_x = 20 + (random(2) ? w_x : -w_x);
-      uint8_t c = EBUS_4BIT_CYAN;
-      uint8_t rnd = random(256);
-      if (rnd < 32) {
-        c = EBUS_4BIT_DARKCYAN;
-      } else if (rnd < 64) {
-        c = EBUS_4BIT_NAVY;
-      }
-      spriteShower.drawLine(w_x, w_y + 12, w_x, w_y + 14, c);
-    }
+// prototypes: update
+void updateDisplay();
+
+// prototypes: draw sprites
+void drawSpriteShower(int32_t x, int32_t y, uint8_t flow);
+void drawSpriteHeater(int32_t x, int32_t y, bool on);
+void drawSpriteWifiStrength(int32_t x, int32_t y, int32_t rssi);
+void drawSpriteEbusQueue(int32_t x, int32_t y, uint8_t queue_size);
+
+//prototypes: helpers
+size_t print_time(struct tm * timeinfo, const char * format);
+void print_ip_addr();
+
+// public functions
+void displayTask(void *pvParameter) {
+  for (;;) {
+    setupDisplay();
+    updateDisplay();
+    vTaskDelay(pdMS_TO_TICKS(100));
   }
-  spriteShower.pushSprite(x, y);
 }
 
-void initSpriteHeater() {
-    spriteHeater.setColorDepth(4);
-    spriteHeater.createSprite(30,30);
-    spriteHeater.createPalette(palette);
-    spriteHeater.fillSprite(EBUS_4BIT_BLACK);
-}
-
-void drawSpriteHeater(int32_t x, int32_t y, bool on) {
-  const uint16_t WAVE_Y_OFFSET = 6;
-  spriteHeater.fillRect(0, 0, 30, WAVE_Y_OFFSET + 1, EBUS_4BIT_BLACK);
-  for (int i = 0; i < 4; i++) {
-    spriteHeater.drawRoundRect(8 * i, WAVE_Y_OFFSET, 5, 24, 1, on ? EBUS_4BIT_ORANGE : EBUS_4BIT_WHITE);
-    if (i < 3) {
-      spriteHeater.drawFastHLine(8 * i + 5, WAVE_Y_OFFSET + 5, 2, on ? EBUS_4BIT_ORANGE : EBUS_4BIT_WHITE);
-      spriteHeater.drawFastHLine(8 * i + 5, WAVE_Y_OFFSET + 20, 2, on ? EBUS_4BIT_ORANGE : EBUS_4BIT_WHITE);
-    }
-  }
-  if (on) {
-    uint8_t wave_y_prev = WAVE_Y_OFFSET - 3, wave_width = 29;
-    for (int8_t wave_x = -1; wave_x < wave_width; wave_x++) {
-      uint8_t wave_y = WAVE_Y_OFFSET - 4 + sinf((wave_x + heaterWaveOffset)/ 2.0) * 2;
-      spriteHeater.drawRect(wave_x - 1, wave_y_prev, 2, 2, EBUS_4BIT_RED);
-      wave_y_prev = wave_y;
-    }
-    heaterWaveOffset++;
-  }
-  spriteHeater.pushSprite(x, y);
-}
-
-void initSpriteWifiStrength() {
-    spriteWifiStrength.setColorDepth(4);
-    spriteWifiStrength.createSprite(16, 16);
-    spriteWifiStrength.createPalette(palette);
-    spriteWifiStrength.fillSprite(EBUS_4BIT_BLACK);
-}
-
-void drawSpriteWifiStrength(int32_t x, int32_t y, int32_t rssi) {
-  uint8_t lvl = 0;
-  if (rssi >= -50) {
-    lvl = 4;
-  } else if (rssi >= -60) {
-    lvl = 3;
-  } else if (rssi >= -70) {
-    lvl = 2;
-  } else if (rssi >= -80) {
-    lvl = 1;
-  }
-  // draw red cross when no signal, overwrite in black otherwise
-  uint32_t c = rssi == WIFI_NO_SIGNAL ? EBUS_4BIT_RED : EBUS_4BIT_BLACK;
-  spriteWifiStrength.drawLine(0, 0, 8, 8, c);
-  spriteWifiStrength.drawLine(1, 0, 9, 8, c);
-  spriteWifiStrength.drawLine(8, 0, 0, 8, c);
-  spriteWifiStrength.drawLine(9, 0, 1, 8, c);
-  for (uint8_t i = 0; i < 4; i++) {
-    uint8_t h = 3 * (i + 1);
-    spriteWifiStrength.fillRect(3 * i + 4, 16 - h, 2, h, lvl > i ? EBUS_4BIT_GREEN : EBUS_4BIT_DARKGREY);
-  }
-  spriteWifiStrength.pushSprite(x, y);
-}
-
-void initSpriteEbusQueue() {
-  spriteEbusQueue.setColorDepth(4);
-  spriteEbusQueue.createSprite(16, 16);
-  spriteEbusQueue.createPalette(palette);
-  spriteEbusQueue.fillSprite(EBUS_4BIT_BLACK);
-}
-
-void drawSpriteEbusQueue(int32_t x, int32_t y, uint8_t queue_size) {
-  uint8_t cutoff = queue_size * 16 / EBUS_TELEGRAM_SEND_QUEUE_SIZE;
-  for (uint8_t i = 0; i < 16; i++) {
-    uint32_t color = EBUS_4BIT_GREEN;
-    if (i > cutoff) {
-      color = EBUS_4BIT_BLACK;
-    } else if (i >= 12) {
-      color = EBUS_4BIT_RED;
-    } else if (i >= 8) {
-      color = EBUS_4BIT_ORANGE;
-    } else if (i >= 4) {
-      color = EBUS_4BIT_YELLOW;
-    }
-    spriteEbusQueue.drawFastVLine(i, 7, 8, color);
-  }
-  spriteEbusQueue.pushSprite(x, y);
-}
-
-void initSprites() {
-  init4BitPalette();
-  initSpriteShower();
-  initSpriteHeater();
-  initSpriteWifiStrength();
-  initSpriteEbusQueue();
-}
-
+// implementations
 void setupDisplay() {
   if (displayInit) {
     return;
@@ -200,20 +111,40 @@ void setupDisplay() {
   displayInit = true;
 }
 
-size_t print_time(struct tm * timeinfo, const char * format) {
-    char buf[64];
-    size_t written = strftime(buf, sizeof(buf), format, timeinfo);
-    if (written == 0) {
-      return written;
-    }
-    return tft.printf(buf);
+void initSprites() {
+  init4BitPalette();
+  initSpriteShower();
+  initSpriteHeater();
+  initSpriteWifiStrength();
+  initSpriteEbusQueue();
 }
 
-#define GET_BYTE(INT32, I) ((INT32 >> 8 * I) & 0XFF)
+void initSpriteShower() {
+    spriteShower.setColorDepth(4);
+    spriteShower.createSprite(30,30);
+    spriteShower.createPalette(palette);
+    spriteShower.fillSprite(EBUS_4BIT_BLACK);
+}
 
-void print_ip_addr() {
-  uint32_t ip = system_info->wifi.ip_addr;
-  tft.printf("%d.%d.%d.%d", GET_BYTE(ip, 0), GET_BYTE(ip, 1), GET_BYTE(ip, 2), GET_BYTE(ip, 3));
+void initSpriteHeater() {
+    spriteHeater.setColorDepth(4);
+    spriteHeater.createSprite(30,30);
+    spriteHeater.createPalette(palette);
+    spriteHeater.fillSprite(EBUS_4BIT_BLACK);
+}
+
+void initSpriteWifiStrength() {
+    spriteWifiStrength.setColorDepth(4);
+    spriteWifiStrength.createSprite(16, 16);
+    spriteWifiStrength.createPalette(palette);
+    spriteWifiStrength.fillSprite(EBUS_4BIT_BLACK);
+}
+
+void initSpriteEbusQueue() {
+  spriteEbusQueue.setColorDepth(4);
+  spriteEbusQueue.createSprite(16, 16);
+  spriteEbusQueue.createPalette(palette);
+  spriteEbusQueue.fillSprite(EBUS_4BIT_BLACK);
 }
 
 void updateDisplay() {
@@ -253,11 +184,110 @@ void updateDisplay() {
   drawSpriteEbusQueue(240, 90, system_info->ebus.queue_size);
 }
 
-void displayTask(void *pvParameter) {
-  for (;;) {
-    setupDisplay();
-    updateDisplay();
-    vTaskDelay(pdMS_TO_TICKS(100));
-  }
+void drawSpriteShower(int32_t x, int32_t y, uint8_t flow) {
+  // remove previous water
+  spriteShower.fillRect(10, 12, 20, 18, EBUS_4BIT_BLACK);
 
+  spriteShower.fillRoundRect(16, 0, 8, 12, 1, flow ? EBUS_4BIT_RED : EBUS_4BIT_BLACK);
+  spriteShower.fillRoundRect(0, 2, 14, 4, 1, flow ? EBUS_4BIT_RED : EBUS_4BIT_BLACK);
+  if (!flow) {
+    spriteShower.drawRoundRect(16, 0, 8, 12, 1, EBUS_4BIT_WHITE);
+    spriteShower.drawRoundRect(0, 2, 14, 4, 1, EBUS_4BIT_WHITE);
+  } else {
+    for (uint8_t i = 0; i < 2 + (flow / 2); i++) {
+      uint16_t w_y = random(16);
+      uint16_t w_x = random((flow * w_y  / 512) + 3);
+      w_x = 20 + (random(2) ? w_x : -w_x);
+      uint8_t c = EBUS_4BIT_CYAN;
+      uint8_t rnd = random(256);
+      if (rnd < 32) {
+        c = EBUS_4BIT_DARKCYAN;
+      } else if (rnd < 64) {
+        c = EBUS_4BIT_NAVY;
+      }
+      spriteShower.drawLine(w_x, w_y + 12, w_x, w_y + 14, c);
+    }
+  }
+  spriteShower.pushSprite(x, y);
 }
+
+void drawSpriteHeater(int32_t x, int32_t y, bool on) {
+  const uint16_t WAVE_Y_OFFSET = 6;
+  spriteHeater.fillRect(0, 0, 30, WAVE_Y_OFFSET + 1, EBUS_4BIT_BLACK);
+  for (int i = 0; i < 4; i++) {
+    spriteHeater.drawRoundRect(8 * i, WAVE_Y_OFFSET, 5, 24, 1, on ? EBUS_4BIT_ORANGE : EBUS_4BIT_WHITE);
+    if (i < 3) {
+      spriteHeater.drawFastHLine(8 * i + 5, WAVE_Y_OFFSET + 5, 2, on ? EBUS_4BIT_ORANGE : EBUS_4BIT_WHITE);
+      spriteHeater.drawFastHLine(8 * i + 5, WAVE_Y_OFFSET + 20, 2, on ? EBUS_4BIT_ORANGE : EBUS_4BIT_WHITE);
+    }
+  }
+  if (on) {
+    uint8_t wave_y_prev = WAVE_Y_OFFSET - 3, wave_width = 29;
+    for (int8_t wave_x = -1; wave_x < wave_width; wave_x++) {
+      uint8_t wave_y = WAVE_Y_OFFSET - 4 + sinf((wave_x + heaterWaveOffset)/ 2.0) * 2;
+      spriteHeater.drawRect(wave_x - 1, wave_y_prev, 2, 2, EBUS_4BIT_RED);
+      wave_y_prev = wave_y;
+    }
+    heaterWaveOffset++;
+  }
+  spriteHeater.pushSprite(x, y);
+}
+
+void drawSpriteWifiStrength(int32_t x, int32_t y, int32_t rssi) {
+  uint8_t lvl = 0;
+  if (rssi >= -50) {
+    lvl = 4;
+  } else if (rssi >= -60) {
+    lvl = 3;
+  } else if (rssi >= -70) {
+    lvl = 2;
+  } else if (rssi >= -80) {
+    lvl = 1;
+  }
+  // draw red cross when no signal, overwrite in black otherwise
+  uint32_t c = rssi == WIFI_NO_SIGNAL ? EBUS_4BIT_RED : EBUS_4BIT_BLACK;
+  spriteWifiStrength.drawLine(0, 0, 8, 8, c);
+  spriteWifiStrength.drawLine(1, 0, 9, 8, c);
+  spriteWifiStrength.drawLine(8, 0, 0, 8, c);
+  spriteWifiStrength.drawLine(9, 0, 1, 8, c);
+  for (uint8_t i = 0; i < 4; i++) {
+    uint8_t h = 3 * (i + 1);
+    spriteWifiStrength.fillRect(3 * i + 4, 16 - h, 2, h, lvl > i ? EBUS_4BIT_GREEN : EBUS_4BIT_DARKGREY);
+  }
+  spriteWifiStrength.pushSprite(x, y);
+}
+
+void drawSpriteEbusQueue(int32_t x, int32_t y, uint8_t queue_size) {
+  uint8_t cutoff = queue_size * 16 / EBUS_TELEGRAM_SEND_QUEUE_SIZE;
+  for (uint8_t i = 0; i < 16; i++) {
+    uint32_t color = EBUS_4BIT_GREEN;
+    if (i > cutoff) {
+      color = EBUS_4BIT_BLACK;
+    } else if (i >= 12) {
+      color = EBUS_4BIT_RED;
+    } else if (i >= 8) {
+      color = EBUS_4BIT_ORANGE;
+    } else if (i >= 4) {
+      color = EBUS_4BIT_YELLOW;
+    }
+    spriteEbusQueue.drawFastVLine(i, 7, 8, color);
+  }
+  spriteEbusQueue.pushSprite(x, y);
+}
+
+size_t print_time(struct tm * timeinfo, const char * format) {
+    char buf[64];
+    size_t written = strftime(buf, sizeof(buf), format, timeinfo);
+    if (written == 0) {
+      return written;
+    }
+    return tft.printf(buf);
+}
+
+
+void print_ip_addr() {
+  uint32_t ip = system_info->wifi.ip_addr;
+  tft.printf("%d.%d.%d.%d", GET_BYTE(ip, 0), GET_BYTE(ip, 1), GET_BYTE(ip, 2), GET_BYTE(ip, 3));
+}
+
+
