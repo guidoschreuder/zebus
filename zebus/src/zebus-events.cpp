a169066021163ad47b2559a1b0abf2c76b3fd740
Guido Schreuder <guido@ruimtepuin.be>
2021-10-19 00:33:36 +0200

    Disable code to periodically shutdown WiFi

--- zebus/src/zebus-events.cpp
+++ zebus/src/zebus-events.cpp
@@ -3,7 +3,7 @@
 void eventTask(void *pvParameter) {
   EventGroupHandle_t event_group = (EventGroupHandle_t) pvParameter;
   // TODO: toggling if for demonstration purposes only
-  bool displayOn = true, wiFiOn = false;
+  bool displayOn = true, wiFiOn = true;
   for(;;) {
     if (displayOn) {
       xEventGroupSetBits(event_group, DISPLAY_ENABLED);
@@ -16,7 +16,7 @@ void eventTask(void *pvParameter) {
       xEventGroupClearBits(event_group, WIFI_ENABLED);
     }
     displayOn = !displayOn;
-    wiFiOn = !wiFiOn;
+    //wiFiOn = !wiFiOn;
     vTaskDelay(pdMS_TO_TICKS(20000));
   }
 }
